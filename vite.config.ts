05cbe34b335495bab66e3b980dcfe9cd6b215941
Muhammadaziz Yakubov <166822077+Muhammadaziz-Yakubov@users.noreply.github.com>
2025-10-24 21:12:02 +0500

    Refactor vite.config.ts for clarity and consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    target: 'esnext',
-    outDir: 'dist', // ⚠️ bu yerda 'dist' bo‘lishi shart
+    outDir: 'dist', // ✅ bu joy muhim!
   },
   server: {
     port: 3000,
     open: true,
   },
-});
+})
